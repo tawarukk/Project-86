8866f35bd0f6b00848f01343734286dfff9a875c
tawarukk <tawarukk@gmail.com>
2024-01-10 00:40:38 +0700

    Saveelement-plus

--- src/main.js
+++ src/main.js
@@ -7,8 +7,11 @@ import 'bootstrap/dist/js/bootstrap.js'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import '@popperjs/core';
 import router from './router';
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 
 const app = createApp(App);
 app.use(router);
+app.use(ElementPlus);
 app.mount('#app');
 // createApp(App).use(router).use(Vuesax).mount('#app')
--- src/views/NewsView.vue
+++ src/views/NewsView.vue
@@ -2,13 +2,14 @@
     <div class="news">
     <nav-bar/>
     <BoxNews/>
+
     </div>
 </template>
     
 <script>
     import  NavBar  from '@/components/Box_SubComponent/NavMain'
     import  BoxNews from '@/components/BoxNews.vue'
-    
+
     export default {
     name: 'NewsView',
     components: {
